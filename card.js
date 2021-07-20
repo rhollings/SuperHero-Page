d719e8e522374256be9e49d55a2b604856c45b01
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-07-20 11:37:12 +0200

    Add files via upload
    cards to show char
    index holds initial page

---
+++ card.js
@@ -0,0 +1,38 @@
+//when page is loaded, it executes function to load teh content based on id received in query params
+window.addEventListener('load', function (req) {
+    //accessing id through query params
+    const urlParams = new URLSearchParams(window.location.search);
+    let id = urlParams.get('id');
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('get', `https://superheroapi.com/api.php/3621675444603381/${id}`);
+    xhr.send();
+    xhr.onload = searchAndUpdate;
+    xhr.onerror = function (err) { console.log(err.responseText); };
+});
+
+//searching for the particular hero and updating its info to the DOM
+function searchAndUpdate(data) {
+
+    //converting response to JSON format
+    data = JSON.parse(data.target.response);
+
+    let heroName = document.getElementById("hero-name");
+    let heroImg = document.getElementById("hero-img");
+    let heroDetail = document.getElementById("hero-detail");
+
+    heroName.innerHTML = `${data.name}`;
+    heroImg.innerHTML = `<img src="${data.image.url}" alt="${data.name}">`;
+    heroDetail.children[0].innerHTML += ` ${data.biography["full-name"] || "Not Available"}`;
+    heroDetail.children[1].innerHTML += ` ${data.biography["place-of-birth"] || "Not Available"}`;
+    heroDetail.children[2].innerHTML += ` ${data.biography.publisher || "Not Available"}`;
+    heroDetail.children[3].innerHTML += ` ${data.biography["first-appearance"] || "Not Available"}`;
+    heroDetail.children[4].innerHTML += ` ${data.appearance.gender || "Not Available"}`;
+    heroDetail.children[5].innerHTML += ` ${data.appearance.height[1] || data.appearance.height[0] || "Not Available"}`;
+    heroDetail.children[6].innerHTML += ` ${data.appearance.weight[1] || data.appearance.weight[0] || "Not Available"}`;
+    heroDetail.children[7].innerHTML += ` ${data.powerstats.intelligence || "Not Available"}`;
+    heroDetail.children[8].innerHTML += ` ${data.powerstats.strength || "Not Available"}`;
+    heroDetail.children[9].innerHTML += ` ${data.powerstats.power || "Not Available"}`;
+    heroDetail.children[10].innerHTML += ` ${data.powerstats.durability || "Not Available"}`;
+    heroDetail.children[11].innerHTML += ` ${data.powerstats.combat || "Not Available"}`;
+}
--- script.js
+++ script.js
@@ -17,7 +17,7 @@ fetch('http://example.com/movies.json')
   .then(response => response.json())
   .then(data => console.log(data));
 
-*/
+
 
 // api url
 const api_url =
@@ -38,7 +38,8 @@ async function getapi(url) {
 		hideloader();
 	}
 	show('response');
-    
+
+	document.getElementById('heros').innerHTML = blah.powerstats.power;
 }
 // Calling that async function
 getapi(api_url);
@@ -47,6 +48,11 @@ getapi(api_url);
 function hideloader() {
 	document.getElementById('loading').style.display = 'none';
 }
+
+
+
+
+
 // Function to define innerHTML for HTML table
 function show(data) {
 	let tab =
@@ -68,3 +74,63 @@ function show(data) {
 	// Setting innerHTML as tab variable
 	document.getElementById("hero").innerHTML = tab;
 }
+
+*/
+
+const charactersList = document.getElementById('charactersList');
+let hpCharacters = {};
+let results = [];
+let favoritesArray = JSON.parse(localStorage.getItem("favoritesArray"));
+let specificChar = {};
+
+const addToFavorites = (id) => {
+    favoritesArray.push(id);
+    alert("added to favorites");
+    localStorage.setItem("favoritesArray", JSON.stringify(favoritesArray));
+};
+
+const loadCharacters = async () => {
+    let search = document.getElementById('searchBar').value;
+    console.log(search);
+    try {
+        const res = await fetch(`https://www.superheroapi.com/api.php/3976831975725162/search/${search}`);
+        hpCharacters = await res.json();
+        results = hpCharacters.results;
+        // console.log(results);
+        displayCharacters(results);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+
+
+
+
+const displayCharacters = (characters) => {
+    // console.log(characters);
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character"><a href="./profile.html?id=${character.id}">
+                <h2 id="character-name">${character.name}</h2></a>
+                 <p id="character-id">id: ${character.id}</p>
+                 <img class="character-image" src="${character.image.url}"></img>
+<h3 class="fullname">${character.appearance.gender}</h3>
+       
+      <h3 class="publisher">${character.biography.publisher}</h3>
+      <button class="btn add-to-fav" data-id=${character.id} onClick="addToFavorites(${character.id})">
+                            Add to favourites
+                        </button>
+                        
+      </li>
+      
+        `
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+
+
+document.getElementById('searchBar').addEventListener("keyup", loadCharacters);
